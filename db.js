4f66cd3c7e932fc1d1a2a553efba4c800cbd1614
Andrew <andrew.goldman00@gmail.com>
2020-06-29 15:42:09 -0600

    got connection working

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Pool = require("pg").Pool;
 const db = new Pool({
   database: "gmdb",
+  user: "gmdb_app",
   password: "123",
   host: "localhost",
   port: 5433,
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const express = require("express");
 const db = require("./db");
-//const { Router } = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", function (req, res, next) {
+  db.query("SELECT * FROM movies LIMIT 5", (err, results) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      res.status(200).json(results.rows);
+    }
+  });
+});
+
+app.listen(PORT);
+
+module.exports = app;

a7efa9ff70c8f1c582ae521ff86c7de619eb0e78
Andrew <andrew.goldman00@gmail.com>
2020-06-30 08:15:48 -0600

    first student endpoint works

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/", function (req, res, next) {
-  db.query("SELECT * FROM movies LIMIT 5", (err, results) => {
-    if (err) {
-      console.log(err.stack);
-    } else {
+app.get("/student", function (req, res, next) {
+  db.query(
+    "SELECT * FROM student ORDER BY student_id ASC",
+    (error, results) => {
+      if (error) {
+        console.log(error.stack);
+      }
       res.status(200).json(results.rows);
     }
-  });
+  );
 });
 
 app.listen(PORT);

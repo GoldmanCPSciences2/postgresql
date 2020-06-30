e5f196b58064a9b23abd09eca4fef95a697f2663
Andrew <andrew.goldman00@gmail.com>
2020-06-30 09:47:42 -0600

    got grades working

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/student", function (req, res, next) {
+app.get("/students", function (req, res, next) {
   db.query(
     "SELECT * FROM student ORDER BY student_id ASC",
     (error, results) => {
@@ -20,7 +20,7 @@ app.get("/student", function (req, res, next) {
   );
 });
 
-app.get("/student/:id", function (req, res, next) {
+app.get("/students/:id", function (req, res, next) {
   const studentid = parseInt(req.params.id, 10);
   db.query(
     `SELECT * FROM student WHERE student_id = ${studentid}`,
@@ -31,6 +31,19 @@ app.get("/student/:id", function (req, res, next) {
       res.status(200).json(results.rows);
     }
   );
+
+  app.get("/grades/:id", function (req, res, next) {
+    const studentid = parseInt(req.params.id, 10);
+    db.query(
+      `SELECT grade FROM grade WHERE student_id = ${studentid}`,
+      (error, results) => {
+        if (error) {
+          console.log(error.stack);
+        }
+        res.status(200).json(results.rows);
+      }
+    );
+  });
 });
 
 app.listen(PORT);
